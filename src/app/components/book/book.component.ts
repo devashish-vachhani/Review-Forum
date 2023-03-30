1fcbe9f2b8f2adbe5fcdb4f3953261f324cfb0c0
Avi-Choksi <Avi0210c@gmail.com>
2023-03-29 22:02:11 -0400

    Implemented basic UI for the book page
    Note: Ratings and Reviews not implemented yet

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Observable, Subscription, tap } from 'rxjs';
+import { DocumentData } from 'firebase/firestore';
+import { Book } from 'src/app/models/book';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-book',
@@ -6,5 +12,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent {
+  book: Book
+  books: Book[];
+  subscription: Subscription;
 
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute,
+  ) {}
+  
+  ngOnInit(): void {
+    this.route.queryParams
+    .subscribe(params => {
+      this.subscription = this.bookService.getBook(params['id']).subscribe(book => {
+        this.book = (book as Book);
+      });
+    });
+  };
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
