ec0f86c3239ff936fce2204a4f729b6c0a49197a
Devashish Vachhani <dvachha@ncsu.edu>
2023-03-30 23:19:31 -0400

    changed return type of get in book service

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/services/book.service';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/models/book';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-book',
@@ -20,12 +20,14 @@ export class BookComponent implements OnInit {
   ) {}
   
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.bookId = params.get('id');
-      this.subscription = this.bookService.getBook(this.bookId).subscribe(book => {
-        this.book = (book as Book);
-      });
-    })
+    this.subscription = this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        this.bookId = params.get('id');
+        return this.bookService.getBook(this.bookId)
+      })
+    )
+    .subscribe(book => this.book = book);
   };
 
   ngOnDestroy(): void {
