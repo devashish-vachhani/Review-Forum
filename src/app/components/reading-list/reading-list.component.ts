ef1caf5371e036b3647674e1aa6233cd06f9b128
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-01 14:06:49 -0400

    base UI of reading-list

--- src/app/components/reading-list/reading-list.component.ts
+++ src/app/components/reading-list/reading-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReadingListService } from '../../services/reading-list.service';
+import { Observable } from 'rxjs';
+import { ReadingList } from 'src/app/models/reading-list';
 
 @Component({
   selector: 'app-reading-list',
   templateUrl: './reading-list.component.html',
   styleUrls: ['./reading-list.component.css']
 })
-export class ReadingListComponent {
+export class ReadingListComponent implements OnInit {
 
+  constructor(
+    private readingListService: ReadingListService,
+  ) {}
+
+  readingList$: Observable<ReadingList>;
+
+  ngOnInit() {
+    this.readingList$ = this.readingListService.getReadingList();
+  }
 }
