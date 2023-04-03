ba63377110fb4b8e1030318d80ffb3a5141983f8
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-03 19:01:23 -0400

    added CRUD for review service

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Firestore, addDoc, collection, collectionData, doc } from '@angular/fire/firestore';
+import { Observable, map } from 'rxjs';
+import { Review } from '../models/review';
+import { deleteDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
 
-  constructor() { }
+  constructor(
+    private firestore: Firestore,
+  ) { }
+
+  getReviews(bookId: string): Observable<Review[]> {
+    const reviewsCollectionRef = collection(this.firestore, `books/${bookId}/reviews`);
+    return collectionData(reviewsCollectionRef, { idField: 'id' })
+            .pipe(
+              map(reviews => {
+                return reviews as Review[];
+              })
+            )
+  }
+
+  async addReview(bookId: string, review: Review): Promise<void> {
+    const reviewsCollectionRef = collection(this.firestore, `books/${bookId}/reviews`);
+    await addDoc(reviewsCollectionRef, review);
+  }
+
+  async deleteReview(bookId: string, reviewId: string): Promise<void> {
+    const reviewDocumentRef = doc(this.firestore, `books/${bookId}/reviews`, reviewId);
+    await deleteDoc(reviewDocumentRef);
+  }
 }
