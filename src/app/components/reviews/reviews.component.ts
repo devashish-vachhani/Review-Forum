bb1cc9022e7cea6743b2c19b2283d7058a84cc51
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-06 00:20:12 -0400

    refactored the code

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, distinctUntilChanged } from 'rxjs';
 import { Review } from 'src/app/models/review';
 import { ReviewService } from 'src/app/services/review.service';
 
@@ -8,62 +8,39 @@ import { ReviewService } from 'src/app/services/review.service';
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.scss']
 })
-export class ReviewsComponent implements OnInit {
+export class ReviewsComponent implements OnInit, OnDestroy {
   @Input('bookId') bookId;
-  fiveCount: number
-  fourCount: number
-  threeCount: number
-  twoCount: number
-  oneCount: number
-  total: number
-  average: string
-
+  reviews: Review[];
+  ratingStats: number[] = [0,0,0,0,0];
+  subscription: Subscription;
 
   constructor(
     private reviewService: ReviewService,
   ) {}
 
-  reviews$: Observable<Review[]>;
-
   ngOnInit(): void {
-    let five: number, four: number, three: number, two: number, one: number
-    this.reviews$ = this.reviewService.getReviews(this.bookId);
-    this.reviews$.subscribe(data => {
-      five = 0
-      four = 0
-      three = 0
-      two = 0
-      one = 0
-      data.forEach(function(review) {
-        if (review['rating'] == 5) {
-          five++
-        }
-        if (review['rating'] == 4) {
-          four++
-        }
-        if (review['rating'] == 3) {
-          three++
-        }
-        if (review['rating'] == 2) {
-          two++
-        }
-        if (review['rating'] == 1) {
-          one++
-        }
-      })
-      this.fiveCount = five
-      this.fourCount = four
-      this.threeCount = three
-      this.twoCount = two
-      this.oneCount = one
-      this.total = five + four + three + two + one
-      this.average = ((five*5 + four*4 + three*3 + two*2 + one) / this.total).toFixed(1)
-    })
+    this.subscription = this.reviewService.getReviews(this.bookId)
+                        .subscribe(reviews => {
+                          this.reviews = reviews;
+                          reviews.forEach((review) => {
+                            this.ratingStats[review.rating - 1]++;
+                          });
+    });
   }
 
-  getPercent(number, total){
-    console.log(((number/total) * 100).toFixed(0).toString())
-    return ((number/total) * 100).toFixed(0).toString()
+  convertToPercent(number) {
+    return ((number / this.reviews.length) * 100).toFixed(0).toString();
+  }
+
+  calculateAverageRating() {
+    const sum = this.ratingStats.reduce((accumulator, value, index) => accumulator + (value*(index+1)), 0);
+    const len = this.reviews.length;
+    const avg = sum/len;
+    return avg.toFixed(1).toString();
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 
 }
