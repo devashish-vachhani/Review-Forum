7d42c56db0fb47db069562bf4178b7b07b628f4c
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-06 15:42:07 -0400

    Refactor ReadingList class to make books property private and add getters and setters

--- src/app/models/reading-list.ts
+++ src/app/models/reading-list.ts
@@ -1,9 +1,21 @@
 import { Book } from "./book";
 
 export class ReadingList {
-    constructor(public books: Book[]) {}
+    private _books: Book[];
 
-    hasBook(bookId: string): boolean {
-        return this.books.some(readingListBook => readingListBook.id === bookId)
+    constructor(books: Book[]) {
+        this._books = books;
+    }
+
+    public get books(): Book[] {
+        return this._books;
+    }
+
+    public set books(books: Book[]) {
+        this._books = books;
+    }
+
+    public hasBook(bookId: string): boolean {
+        return this._books.some(readingListBook => readingListBook.id === bookId);
     }
 }
