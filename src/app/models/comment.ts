30c94f5ef70c12f143efec57b5785cec0341586d
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-07 18:24:11 -0400

    add CRUD for comment service

---
+++ src/app/models/comment.ts
@@ -0,0 +1,55 @@
+export class Comment {
+    private _id?: string;
+    private _commenter: string;
+    private _text: string;
+    private _date: any;
+  
+    constructor(commenter: string, text: string, date: Date, id?: string) {
+      this._id = id;
+      this._commenter = commenter;
+      this._text = text;
+      this._date = date;
+    }
+  
+    get id(): string {
+      return this._id;
+    }
+  
+    get commenter(): string {
+      return this._commenter;
+    }
+  
+    get text(): string {
+      return this._text;
+    }
+  
+    get date(): any {
+      return this._date;
+    }
+  
+    // setters
+    set id(value: string) {
+      this._id = value;
+    }
+  
+    set commenter(value: string) {
+      this._commenter = value;
+    }
+  
+    set text(value: string) {
+      this._text = value;
+    }
+  
+    set date(value: any) {
+      this._date = value;
+    }
+  
+    toJson(): any {
+      return {
+        id: this._id,
+        commenter: this._commenter,
+        text: this._text,
+        date: this._date,
+      };
+    }
+  }  
