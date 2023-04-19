814cbc8fea314b4e8fc85d86c4d9370575296a6f
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-18 20:33:09 -0400

    add config for cypress coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   
-  e2e: {
+  e2e: {    
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
     'baseUrl': 'http://localhost:4200'
   },
   
