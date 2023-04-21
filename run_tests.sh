0244ac870ff8283b52d73b9e00ef58ab8ef493ad
Devashish Vachhani <dvachha@ncsu.edu>
2023-04-20 23:28:22 -0400

    final change

--- run_tests.sh
+++ run_tests.sh
@@ -13,11 +13,11 @@ sleep 120
 docker exec -it $CONTAINER_ID sh -c 'npm run test:ci && npm run cypress:run && npx istanbul-merge --out ./coverage/combined/coverage-final.json ./coverage/cypress/coverage-final.json ./coverage/karma/coverage-final.json && npx istanbul report json-summary --dir=./coverage/combined --include=coverage/combined/coverage-final.json && npm run coverage:badge'
 
 # Copy coverage and readme files to local machine
-docker cp $CONTAINER_ID:/app/coverage/combined/coverage-summary.json ./coverage/coverage-summary.json
+rm -rf coverage && docker cp $CONTAINER_ID:/app/coverage ./coverage
 docker cp $CONTAINER_ID:/app/README.md ./README.md
 
 # Set env variables for coverage and readme files
-echo "::set-env name=COVERAGE_PATH::$(pwd)/coverage/coverage-summary.json"
+echo "::set-env name=COVERAGE_PATH::$(pwd)/coverage/combined/coverage-summary.json"
 echo "::set-env name=README_PATH::$(pwd)/README.md"
 
 # Delete the container
